4c3475e361cea0422aae98b87e4d1851516567a5
Alex <you@example.com>
2019-03-31 22:42:30 +0300

    add authorization

---
+++ app/controllers/CustomErrorHandler.java
@@ -0,0 +1,31 @@
+package controllers;
+
+
+import play.*;
+import play.api.OptionalSourceMapper;
+import play.api.UsefulException;
+import play.api.routing.Router;
+import play.http.DefaultHttpErrorHandler;
+import play.mvc.Http.*;
+import play.mvc.*;
+
+import javax.inject.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+public class CustomErrorHandler  extends DefaultHttpErrorHandler {
+
+    @Inject
+    public CustomErrorHandler(Configuration configuration, Environment environment,
+                              OptionalSourceMapper sourceMapper, Provider<Router> routes) {
+        super(configuration, environment, sourceMapper, routes);
+    }
+
+    protected CompletionStage<Result> onDevServerError(RequestHeader var1, UsefulException var2) {
+        return CompletableFuture.completedFuture(Results.status(500, "error"));
+    }
+
+    protected CompletionStage<Result> onProdServerError(RequestHeader var1, UsefulException var2) {
+        return CompletableFuture.completedFuture(Results.status(500, "error"));
+    }
+}
---
+++ app/controllers/DemoHttpActionAdapter.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import org.pac4j.core.context.HttpConstants;
+import org.pac4j.play.PlayWebContext;
+import org.pac4j.play.http.DefaultHttpActionAdapter;
+import play.mvc.Result;
+
+import static play.mvc.Results.*;
+
+public class DemoHttpActionAdapter extends DefaultHttpActionAdapter {
+
+    @Override
+    public Result adapt(int code, PlayWebContext context) {
+        if (code == HttpConstants.UNAUTHORIZED) {
+            return unauthorized("unauthorized").as((HttpConstants.HTML_CONTENT_TYPE));
+        } else if (code == HttpConstants.FORBIDDEN) {
+            return forbidden("forbidden").as((HttpConstants.HTML_CONTENT_TYPE));
+        } else {
+            return super.adapt(code, context);
+        }
+    }
+}
