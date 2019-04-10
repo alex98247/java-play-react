347f8cece7c0034624f7bfbf980fe9b11121d7b4
alex <alexmir2015@yandex.ru>
2019-04-10 23:15:44 +0300

    DBGame fix Timestamp

--- app/tools/DBGame.java
+++ app/tools/DBGame.java
@@ -6,8 +6,10 @@ import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
 import models.Game;
+import play.libs.Json;
 
 import java.lang.reflect.Type;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -17,17 +19,22 @@ public class DBGame {
     public List<Game> getGames(int count) {
         int quotient = count / 50;
         int remainder = count - quotient * 50;
-        int timestamp = -631162800;
+        Timestamp timestamp = new Timestamp(-631162800);
+
         List<Game> games;
         games = getButchGames(timestamp, remainder);
+
         for (int i = 0; i < quotient; i++) {
-            timestamp = games.stream().map(x -> x.getCreated_at()).max(Comparator.comparing(Integer::valueOf)).orElse(timestamp);
+            timestamp = games.stream()
+                    .map(x -> x.getCreated_at())
+                    .max(Comparator.comparing(Timestamp::getTime))
+                    .orElse(timestamp);
             games.addAll(getButchGames(timestamp, 50));
         }
         return games;
     }
 
-    private List<Game> getButchGames(int timestamp, int count) {
+    private List<Game> getButchGames(Timestamp timestamp, int count) {
         HttpResponse<JsonNode> jsonResponse = Unirest.post("https://api-v3.igdb.com/games")
                 .header("user-key", "d0fc4e5aa35986706d0b32bb67d615a7")
                 .header("Accept", "application/json")
