708d2c2ee4433fb56d66815d27deb6b922cbce56
Alex <you@example.com>
2019-04-26 15:06:27 +0300

    use Redux Persist for persist the storage

--- ui/src/index.js
+++ ui/src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
-import * as redux from "redux";
-import reducer from "./reducer";
+import { PersistGate } from 'redux-persist/integration/react';
+import configureStore from "./configureStore";
 
-var store = redux.createStore(reducer);
+var store = configureStore();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App/>
+  <Provider store={store.store}>
+    <PersistGate loading={null} persistor={store.persistor}>
+      <App/>
+    </PersistGate>
   </Provider>,
   document.getElementById('root'));
 serviceWorker.register();
