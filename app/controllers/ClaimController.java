5ec40498f67394d92091ab30f62c25b171df2460
alex <alexmir2015@yandex.ru>
2019-05-11 17:51:52 +0300

    add history on claim

--- app/controllers/ClaimController.java
+++ app/controllers/ClaimController.java
@@ -4,30 +4,49 @@ import ch.qos.logback.core.status.ErrorStatus;
 import com.fasterxml.jackson.databind.JsonNode;
 import io.swagger.annotations.*;
 import models.dao.Claim;
+import models.dao.History;
+import org.pac4j.core.profile.CommonProfile;
+import org.pac4j.core.profile.ProfileManager;
+import org.pac4j.play.PlayWebContext;
+import org.pac4j.play.store.PlaySessionStore;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import service.ClaimService;
+import service.HistoryService;
 
 import javax.inject.Inject;
 import java.util.Date;
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.Optional;
 
 @Api(value = "Claim Controller", produces = "application/json")
 public class ClaimController extends Controller {
 
     @Inject
     private ClaimService claimService;
+    @Inject
+    private PlaySessionStore playSessionStore;
+    @Inject
+    private HistoryService historyService;
 
     @ApiOperation(value = "Add Claim", notes = "Add new claim from json")
     public Result addClaim() {
         JsonNode json = request().body().asJson();
-        Claim message = Json.fromJson(json, Claim.class);
-        Date date = new Date();
-        Timestamp ts = new Timestamp(date.getTime());
-        Timestamp empts = new Timestamp(0);
-        Claim claim = new Claim(message.getId(), message.getUser_id(), ts, false, empts, message.getComment(), message.getTheme());
+        Claim claim = Json.fromJson(json, Claim.class);
+
+        PlayWebContext webContext = new PlayWebContext(ctx(), playSessionStore);
+        ProfileManager<CommonProfile> profileManager = new ProfileManager(webContext);
+        Optional<CommonProfile> profileOptional = profileManager.get(true);
+        if (!profileOptional.isPresent()) {
+            //return exception
+        }
+        CommonProfile profile = profileOptional.get();
+        long id = Long.parseLong(profile.getId());
+        History history = new History(id, "Create claim,");
+        historyService.addHistory(history);
+
         claimService.createClaim(claim);
         return ok();
     }
@@ -42,7 +61,7 @@ public class ClaimController extends Controller {
     @ApiOperation(value = "Update Claim", notes = "Update game in list from json")
     @ApiResponses({
             @ApiResponse(code = 404, message = "Claim Not Found", response = ErrorStatus.class),
-            @ApiResponse(code = 500, message = "Internal Server Error", response = ErrorStatus.class) })
+            @ApiResponse(code = 500, message = "Internal Server Error", response = ErrorStatus.class)})
     public Result updateClaim(@ApiParam(value = "Game Id", name = "id") long id) {
         JsonNode json = request().body().asJson();
         Claim message = Json.fromJson(json, Claim.class);
@@ -60,7 +79,7 @@ public class ClaimController extends Controller {
     @ApiOperation(value = "Get All Claims", notes = "Get list of claims", response = JsonNode.class)
     @ApiResponses({
             @ApiResponse(code = 404, message = "Claims Not Found", response = ErrorStatus.class),
-            @ApiResponse(code = 500, message = "Internal Server Error", response = ErrorStatus.class) })
+            @ApiResponse(code = 500, message = "Internal Server Error", response = ErrorStatus.class)})
     public Result getClaims() {
         List<Claim> claims = claimService.getClaims();
         JsonNode jsonNode = Json.toJson(claims);
@@ -70,7 +89,7 @@ public class ClaimController extends Controller {
     @ApiOperation(value = "Get Claim By Id", notes = "Get the claim by it's Id", response = JsonNode.class)
     @ApiResponses({
             @ApiResponse(code = 404, message = "Claim Not Found", response = ErrorStatus.class),
-            @ApiResponse(code = 500, message = "Internal Server Error", response = ErrorStatus.class) })
+            @ApiResponse(code = 500, message = "Internal Server Error", response = ErrorStatus.class)})
     public Result getClaimById(@ApiParam(value = "Game Id", name = "id") long id) {
         Claim claim = claimService.getClaimById(id);
         JsonNode jsonNode = Json.toJson(claim);
